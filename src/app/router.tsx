0c74c01d8a67e106c087f0effbf3ea7c2e654970
Nancy <nghazou25@gmail.com>
2026-01-28 22:03:26 -0400

    Router creado y agregado al main.tsx

---
+++ src/app/router.tsx
@@ -0,0 +1,33 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import PublicLayout from "../components/layout/PublicLayout";
+import DashboardLayout from "../components/layout/DashboardLayout";
+
+import Home from "../pages/ecommerce/Home";
+import Products from "../pages/ecommerce/Products";
+import ProductDetail from "../pages/ecommerce/ProductDetail";
+
+import DashboardHome from "../pages/dashboard/DashboardHome";
+import DashboardProducts from "../pages/dashboard/DashboardProducts";
+
+const AppRouter = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        {/* ECOMMERCE */}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+        </Route>
+
+        {/* DASHBOARD */}
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<DashboardHome />} />
+          <Route path="products" element={<DashboardProducts />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+};
+
+export default AppRouter;
